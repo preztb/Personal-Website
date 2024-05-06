874b34d09876de71ad23695755d55d863ee1dfab
Tyler Beck <tylerbeckmail@gmail.com>
2024-05-06 08:56:31 -0400

    site title + page 2 git logo

--- src/projectcomp/Layproj.jsx
+++ src/projectcomp/Layproj.jsx
@@ -3,15 +3,27 @@ import './proj.css'
 import Image from 'react-bootstrap/Image'
 import Github from '../assets/github-mark.svg'
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/esm/Container'
+import Row from 'react-bootstrap/esm/Row'
+import Col from 'react-bootstrap/esm/Col'
 
 const Layproj = () => {
     
     
   return (
     <div>
-        <Link to='https://github.com/preztb/Personal-Website'>
-            <Image src={Github} alt="github logo"/>
-        </Link>
+        <Container>
+            <Row>
+                <Col>
+                <Link to='https://github.com/preztb/Personal-Website'>
+                    <div className='image-str'>
+                    <Image src={Github} alt="github logo"/>
+                    </div>
+                </Link>
+                </Col>
+            </Row>
+        </Container>
+        
       
     </div>
   )
